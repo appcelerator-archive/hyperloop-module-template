54b07f6bbc6b3706c71e48fbba29270ca7f60d98
Fokke Zandbergen <mail@fokkezb.nl>
2016-03-03 14:12:56 +0100

    Add example of including Swift code

--- Resources/ios/ExampleView.js
+++ Resources/ios/ExampleView.js
@@ -1,6 +1,9 @@
 var UIScreen = require('UIKit/UIScreen'),
   UIView = require("UIKit/UIView");
 
+// The Swift file we have in vendor
+var MySwiftView = require('MyFramework/MySwiftView');
+
 exports.create = function(params) {
   params = params || {};
 
@@ -15,6 +18,12 @@ exports.create = function(params) {
 
     view.setBackgroundColor(uiColor);
   }
+
+  var view2 = new MySwiftView();
+  view2.frame = CGRectMake(0, 0, 128, 128);
+  view2.center = CGPointMake(bounds.size.width / 2, (bounds.size.height / 2) + 50);
+
+  view.add(view2);
 
   return view;
 };
