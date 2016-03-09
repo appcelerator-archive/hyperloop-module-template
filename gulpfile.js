f6d310b120ea8848ed05e9dcdad23410bd50e0f8
Jeff Haynie <jhaynie@gmail.com>
2016-03-09 14:17:36 -0800

    add prepublish for npm. additional gulpfile changes like eslint and mocha, clean. fixed example. updated README

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,67 @@
-var gulp = require('gulp'),
-	babel = require('gulp-babel'),
-	babelRegister = require('babel-register'),
-	mocha = require('gulp-mocha');
+'use strict';
 
-gulp.task('compile', function () {
+var gulp = require('gulp'),
+	$ = require('gulp-load-plugins')(),
+	del = require('del'),
+	babelRegister = require('babel-register');
+
+/**
+ * clean up intermediate directories
+ */
+gulp.task('clean', function (done) {
+	del(['dist', 'lib']).then(function () { done(); });
+});
+
+/**
+ * babel compile our source from ES6 to ES5
+ */
+gulp.task('compile', ['lint-src'], function () {
 	return gulp.src('src/**/*.js')
-		.pipe(babel())
+		.pipe($.plumber())
+		.pipe($.babel())
 		.pipe(gulp.dest('lib'));
 });
 
-gulp.task('compile-tests', function () {
-	return gulp.src('test/**/*.js')
-		.pipe(babel())
-		.pipe(gulp.dest('dist'));
+/*
+ * lint tasks
+ */
+function lint(pattern) {
+	return gulp.src(pattern)
+		.pipe($.plumber())
+		.pipe($.eslint())
+		.pipe($.eslint.format())
+		.pipe($.eslint.failAfterError());
+}
+
+/**
+ * ensure our source code is lint
+ */
+gulp.task('lint-src', function () {
+	return lint('src/**/*.js');
 });
 
-gulp.task('test-exec', ['compile', 'compile-tests'], function () {
-	return gulp.src('dist/*_test.js')
-		.pipe(mocha({
+/**
+ * ensure our test source code is lint
+ */
+gulp.task('lint-test', function () {
+	return lint('test/**/*_test.js');
+});
+
+/**
+ * execute mocha unit tests
+ */
+gulp.task('test-exec', ['lint-test', 'compile'], function () {
+	return gulp.src('test/*_test.js')
+		.pipe($.mocha({
 			reporter: 'nyan',
 			compilers: {js:babelRegister}
 		}));
 });
 
+/**
+ * standard task that will ensure that we run mocha with special
+ * NODE_PATH for our mocks
+ */
 gulp.task('test', function (cb) {
 	if (!process.env.NODE_PATH) {
 		var spawn = require('child_process').spawn;
@@ -31,6 +70,14 @@ gulp.task('test', function (cb) {
 	}
 });
 
-gulp.task('default', ['compile'], function () {
+/**
+ * build tasks which performs the compile and then validates the unit tests
+ */
+gulp.task('build', ['compile', 'test'], function () {
+});
 
+/**
+ * default is to just build
+ */
+gulp.task('default', ['build'], function () {
 });
