0361c22e4f4514aad78589c06a3deeb62f51379e
Jeff Haynie <jhaynie@gmail.com>
2016-03-10 10:42:58 -0800

    remove minsdk in favor of apiversion

--- appc.js
+++ appc.js
@@ -2,32 +2,31 @@
  * Hyperloop configuration
  */
 module.exports = {
-    // Basic configuration
-    type: 'module',
-    group: 'titanium',
-    platforms: {
-        ios: {
-            vendor: {
-                'MyFramework': {
-                     // these can be an array or string
-                     source: ['src'],
-                     header: 'src',
-                     resource: 'src'
-                }
-            }
-        },
-        android: {
-            vendor: {}
-        },
-        windows: {
-            vendor: {}
+  // Basic configuration
+  type: 'module',
+  group: 'titanium',
+  platforms: {
+    ios: {
+      vendor: {
+        'MyFramework': {
+          // these can be an array or string
+          source: ['src'],
+          header: 'src',
+          resource: 'src'
         }
+      }
     },
-		timodule: {
-			minsdk: '6.0.0',
-			apiversion: '1',
-			guid: '35ebe7cc-0854-40f0-b67b-61014d67beaf',
-			moduleid: 'ti.test',
-			platforms: ['ios', 'android']
-		}
+    android: {
+      vendor: {}
+    },
+    windows: {
+      vendor: {}
+    }
+  },
+  timodule: {
+    apiversion: '1',
+    guid: '35ebe7cc-0854-40f0-b67b-61014d67beaf',
+    moduleid: 'ti.test',
+    platforms: ['ios', 'android']
+  }
 }
